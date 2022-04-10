ec8f0c31d60065ad124b20b141000d1284cee4ed
faith <fmuchembi@gmail.com>
2022-04-10 10:06:36 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import './App.css';
+import './app.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route, Routes,Link} from "react-router-dom";
 import Map from './components/Map';
 import TableFormData from './components/TableFormData'
+import {faDrawPolygon, faGripLines, faEllipsisH, faTable, faGlobeAfrica} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+
 
 
 class App extends React.Component{
@@ -13,7 +17,6 @@ class App extends React.Component{
     polygonData:[],
     buttonId:null
   }
-
   showLinesData = async ()=>{
     const response = await fetch("http://collectdata2021.herokuapp.com/lineapi")
     const lineData = await response.json();
@@ -34,6 +37,7 @@ class App extends React.Component{
     setButton(id) {
       this.setState({ buttonId: id });
   }
+ 
   render(){
     const linkstyle ={
     color:'white',
@@ -42,7 +46,15 @@ class App extends React.Component{
     fontWeight:'400',
     letterSpacing:1,
     fontSize:16,
-
+  }
+  const icons ={
+    color:"orangered",
+    fontSize:24,
+    marginRight:"10%" 
+  }
+  const iconTwo ={
+    marginRight:"8%",
+    fontSize:25
   }
     return(
       <div className=''>
@@ -50,19 +62,19 @@ class App extends React.Component{
           <div className="col-2">
                <ul className='list-group list-group-flush'>
                   <li className='list-group-item text-muted mt-5'><h5>Data types</h5></li>
-                  <li className='list-group-item' onClick={() => { this.setButton(0); this.showPointsData()}}>points</li>
-                  <li className='list-group-item' onClick={() => { this.setButton(1); this.showLinesData()}}>Lines</li>
-                  <li className='list-group-item' onClick={() => { this.setButton(2); this.showPolygonData()}}>Polygons</li>
+                  <li className='list-group-item' onClick={() => { this.setButton(0); this.showPointsData()}}><FontAwesomeIcon style={icons} icon={faEllipsisH} onClick={this.showPointsData}/>Points</li>
+                  <li className='list-group-item' onClick={() => { this.setButton(1); this.showLinesData()}}><FontAwesomeIcon style={icons} icon={faGripLines}  onClick={this.showLinesData}/>Lines</li>
+                  <li className='list-group-item' onClick={() => { this.setButton(2); this.showPolygonData()}}><FontAwesomeIcon style={icons} icon={faDrawPolygon} onClick={this.showPolygonData}/>Polygons</li>  
                </ul>
           </div>
           <div className="col-10">
             <Router>
               <div className="App">
                 <div className='buttons2'>
-                    <button type="button" className='btn  btn-sm'><Link to="/" style={linkstyle}>display map</Link></button>
+                    <button type="button" className='btn  btn-sm'><Link to="/" style={linkstyle}> <FontAwesomeIcon style={iconTwo} icon={faGlobeAfrica}/>display map</Link></button>
                  </div>
                  <div className='buttons'>
-                    <button type="button" className='btn  btn-sm'><Link to="/table" style={linkstyle}>display table</Link></button>
+                    <button type="button" className='btn  btn-sm'><Link to="/table" style={linkstyle}><FontAwesomeIcon style={iconTwo} icon={faTable}/>display table</Link></button>
                  </div>
                 <Routes>
                   <Route exact path='/' element={< Map pointData={this.state.pointData} lineData={this.state.lineData} polygonData={this.state.polygonData} buttonId={this.state.buttonId}/>}>
@@ -79,6 +91,7 @@ class App extends React.Component{
 
 }
 export default App;
+
 
 
 
